53c1fe44c9a016ba3d953ff3b8226c8ec6e6dc07
mrebelladofernandez <mrebelladofernandez@nautilus18.danielcastelao.org>
2018-10-30 12:24:52 +0100

    first commit

---
+++ Boletin6_3/src/boletin6_3/PositivoNegativo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin6_3;
+
+import java.util.Scanner;
+
+/**
+ * @since @version @see @author mrebelladofernandez
+ */
+public class PositivoNegativo {
+
+    Scanner res = new Scanner(System.in);
+    int num1;
+
+    public void tecleaNumero() {
+        System.out.println("Inserte o numero:");
+        num1 = res.nextInt();
+        if (num1 > 0) {
+            System.out.println("+");
+        } else if (num1 < 0) {
+            System.out.println("-");
+        } else {
+            System.out.println("0");
+        }
+    }
+
+}
