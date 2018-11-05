9cf33930e32e20331ba60315944af7d6bb862fce
mrebelladofernandez <mrebelladofernandez@nautilus18.danielcastelao.org>
2018-11-05 12:05:16 +0100

    second commit(enunciado e cambio de tipo)

--- Boletin6_5/src/boletin6_5/Boletin6_5.java
+++ Boletin6_5/src/boletin6_5/Boletin6_5.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *Dados 3 números que se supon distintos , indicar cal é o maior.
  */
 package boletin6_5;
 
